b93532b280d73e38abc1a21cf28afa375dda202b
ProNomanRizvi <pro.noman.rizvi@gmail.com>
2026-07-09 11:10:18 +0500

    study: add SQL queries for GROUP BY, ORDER BY, and HAVING tasks in basics.sql

--- phase-4-sql-data-manipulation/week1_sql/basics.sql
+++ phase-4-sql-data-manipulation/week1_sql/basics.sql
@@ -26,3 +26,30 @@ WHERE gpa >= 3.7 AND department = 'SE';
 -- ============================================
 -- Topic 2: GROUP BY, ORDER BY, HAVING
 -- ============================================
+
+-- Task 06: All students in DESC Order according to their GPA
+SELECT * 
+FROM students
+ORDER BY gpa DESC;
+
+-- Task 07: Get Total Count of Every Department
+SELECT department, COUNT(*)
+FROM students
+GROUP BY department;
+
+-- Task 08: Get Average GPA of Every Department 
+SELECT department, AVG(gpa)
+FROM students
+GROUP BY department;
+
+-- Task 09: Only Departments Average GPA > 3.5 
+SELECT department, AVG(gpa)
+FROM students
+GROUP BY department
+HAVING AVG(gpa) > 3.5;
+
+-- Task 10: Average GPA of Every Department in DESC Order
+SELECT department, AVG(gpa)
+FROM students
+GROUP BY department
+ORDER BY AVG(gpa) DESC;
