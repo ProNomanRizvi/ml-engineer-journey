54b741437083e20904ca491eec5d612d7cde8f0d
ProNomanRizvi <pro.noman.rizvi@gmail.com>
2026-07-11 15:25:25 +0500

    Phase 4 Week 1: Window Functions - ROW_NUMBER, RANK, LAG, LEAD, PARTITION BY

---
+++ phase-4-sql-data-manipulation/week1_sql/advanced.sql
@@ -0,0 +1,29 @@
+-- ============================================
+-- Topic: Window Functions, CTEs, Subqueries
+-- ============================================
+
+-- Task 1: Use Row Number
+
+SELECT name, gpa,
+    ROW_NUMBER() OVER(ORDER BY gpa DESC) AS gpa_rank
+FROM students;
+
+-- Task 2: Use Rank
+SELECT name, gpa,
+    RANK() OVER(ORDER BY gpa DESC) AS gpa_rank
+FROM students;
+
+-- Task 3: Use LAG
+SELECT name, gpa,
+    LAG(gpa) OVER(ORDER BY gpa DESC) AS pre_gpa
+FROM students;
+
+-- Task 4: Use LEAD
+SELECT name, gpa,
+    LEAD(gpa) OVER(ORDER BY gpa DESC) AS next_gpa
+FROM students;
+
+-- Task 5: Use PARTITION BY
+SELECT name, department, gpa,
+    RANK() OVER(PARTITION BY department ORDER BY gpa DESC) AS dept_rank
+FROM students;
